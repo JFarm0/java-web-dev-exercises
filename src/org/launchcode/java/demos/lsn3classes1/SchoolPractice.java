e305b7f8ba7ab280974ee7d5a605415ccb133c97
J Farmer <JFarm0>
2021-05-02 21:12:09 -0500

    Finished Class 4 Exercises

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,8 +1,13 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+
+//        Student jared = new Student(name: "Jared", studentId: 1, numberOfCredits: 1, gpa: 4.0);
+
 
         Student jared = new Student();
         jared.setNumberOfCredits(1);
--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -1,5 +1,7 @@
 package org.launchcode.java.demos.lsn4classes2;
 
+import java.util.Objects;
+
 public class Student {
 
     private static int nextStudentId = 1;
@@ -30,20 +32,55 @@ public class Student {
 
 
      //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public String getGradeLevel(int credits) {
+        String studentLevel;
+        if (credits >= 0 && credits <= 29) {
+            studentLevel = "Freshman";
+        } else if (credits >= 30 && credits <= 59) {
+            studentLevel = "Sophomore";
+        } else if (credits >= 60 && credits <= 89) {
+            studentLevel = "Junior";
+        } else {
+            studentLevel = "Senior";
+        }
+        return studentLevel;
+        // Determine the grade level of the student based on numberOfCredits
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
+        double qualityScore;
+        qualityScore = courseCredits * grade;
+        double totalQualityScore;
+        totalQualityScore = getGpa() * getNumberOfCredits();
+        double updatedQualityScore;
+        updatedQualityScore = totalQualityScore + qualityScore;
+        setNumberOfCredits(getNumberOfCredits() + courseCredits);
+        setGpa(updatedQualityScore/getNumberOfCredits());
         // Update the appropriate fields: numberOfCredits, gpa
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
+    public String toString() {
+        return name + " (Credits: " + numberOfCredits + ", GPA: " + gpa + ")";
+    }
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId && name.equals(student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, studentId);
+    }
 
     public String getName() {
         return name;
@@ -85,5 +122,11 @@ public class Student {
         System.out.println(sally);
         sally.addGrade(25, 3.8);
         System.out.println(sally);
+//        System.out.println("Sally has a GPA of " + sally.getGpa() + "on " + sally.getNumberOfCredits() + "credits.");
+//        sally.addGrade(3, 4.0);
+//        System.out.println(sally);
+//        System.out.println("Sally has a GPA of " + sally.getGpa() + "on " + sally.getNumberOfCredits() + "credits.");
+//        System.out.println(sally.getNumberOfCredits());
+//        System.out.println("Sally is a " + sally.getGradeLevel(sally.getNumberOfCredits()));
     }
 }
